7327c2592c26d65f376a6cdb88af259573391a55
myspringfaces <myspringfaces@gmail.com>
2017-08-17 11:09:08 -0700

    related to access contorl and modifier

---
+++ Declarations and Access Modifiers/src/ClassStructure.java
@@ -0,0 +1,18 @@
+class ClassStructure  {
+}
+
+class A
+{
+}
+
+
+class B
+{
+}
+
+
+class C
+{
+}
+//compiling this code has no issues byt if you try to run the coden the compiler will complain an error syaing cannot find main method
+// in this class when you run the code compiler will always look for the main method.,
---
+++ Declarations and Access Modifiers/src/Raghu.java
@@ -0,0 +1,13 @@
+public class A
+{
+}
+
+class Raghu 
+{
+		public static void main(String [] args) {
+			System.out.println(" this is the main method class with out public and name is differ");
+		}
+}
+
+// In the above code the public class is missing the main method the acces modifer publci class should have main method
+// and the name of the class with main mehtod and public should match the java source file this is the basic condition.
