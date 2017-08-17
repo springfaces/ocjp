31e80d854fdf81633c6f6b1db7d8f4843d591e48
myspringfaces <myspringfaces@gmail.com>
2017-08-17 14:57:50 -0700

    updated
    updated

---
+++ Declarations and Access Modifiers/OutPrint.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+
+
+public class OutPrint {
+
+	public static void main(String [] args) {
+		out.print(" I am printing by using just out");	
+	}
+}
---
+++ Declarations and Access Modifiers/src/StaticImportTest.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.sqrt;
+import static java.lang.Math.*;
+
+class StaticImportTest {
+		
+			public static void main(String []args) {
+	
+				System.out.println(sqrt(4));
+					
+			}
+}
